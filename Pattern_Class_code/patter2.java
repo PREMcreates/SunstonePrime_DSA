e7298587475fe7d906e8dc19bde522a260373dee
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-01-19 22:59:58 +0530

    pattern ques at home

--- Pattern_Class_code/patter2.java
+++ Pattern_Class_code/patter2.java
@@ -2,23 +2,52 @@ import java.util.Scanner;
 
 public class patter2 {
     public static void main(String[] args) {
-        try(Scanner sc = new Scanner(System.in)) {
-            int n = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int i, j, n = sc.nextInt();
 
-            //column symmetric
+            // column symmetric
             System.out.println("Column Symmetric");
-            for (int i = 0; i < 10; i++) {
-                System.out.println("*");
+            for (i = 0; i <= n; i++) {
+                for (j = 0; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+
+                for (j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+
+                for (j = 0; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println();
             }
 
-
-            //column asymmetric
+            // column asymmetric
             System.out.println("Column Asymmetric");
-            for (int i = 0; i < 10; i++) {
-                System.out.println("*");
+            for (i = 0; i <= n; i++) {
+                for (j = 0; j <= n-i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+
+                for (j = 0; j <= i-1; j++) {
+                    System.out.print("*");
+                }
+
+                for (j = 0; j <= n-i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println();
             }
 
         }
     }
-    
+
 }
--- Pattern_Class_code/patter3.java
+++ Pattern_Class_code/patter3.java
@@ -2,14 +2,35 @@ import java.util.Scanner;
 public class patter3 {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
-            int n = sc.nextInt();
+            int i, j,  n = sc.nextInt();
 
-            for (int i = 1; i <=n; i++) {
-                for(int j=1; j<=i; j++){
-                    if(j==1 || j==(n-1)){
-                        System.out.print("");
+            for (i = 0; i <= n; i++) {
+                for (j = 0; j <= n-i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (j = 0; j <= i; j++) {
+                    
+                    if(j%2 == 0){
+                        System.out.print("*");
                     }
-                    System.out.print("* ");
+                    if(j%2 != 0){
+                        System.out.print("!");
+                    }
+                }
+
+                for (j = 0; j <= i-1; j++) {
+                    
+                    if(j%2 != 0){
+                        System.out.print("*");
+                    }
+                    if(j%2 != 0){
+                        System.out.print("!");
+                    }
+                }
+
+                for (j = 0; j <= n-i; j++) {
+                    System.out.print("");
                 }
                 System.out.println();
             }
