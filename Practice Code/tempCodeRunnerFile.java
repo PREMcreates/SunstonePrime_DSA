c58d7be17cfeb9c502f40c9391830ff1fbc489d8
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-05 22:39:49 +0530

    test code added

--- Practice Code/tempCodeRunnerFile.java
+++
@@ -1,8 +0,0 @@
-for(j=1; j<=mid-1; j++){
-                    if(i==1 || i==mid){
-                        System.out.print("*");
-                    }
-                    else{
-                    System.out.print(" ");
-                }
-                }
