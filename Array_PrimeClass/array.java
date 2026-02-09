cecc9d343b4a7add3a9a770127ed4027cc3a2b42
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-09 14:06:59 +0530

    array folder added

---
+++ Array_PrimeClass/array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class array {
+    public static void main(String[] args) {
+
+        int[] arr = new int[5];
+        Scanner sc = new Scanner(System.in);
+
+        // taking input
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // output
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
