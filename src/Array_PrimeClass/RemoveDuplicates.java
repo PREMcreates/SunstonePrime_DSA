6a26cc3830d7e2e1391f7650df01f7a4ff705054
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-12 16:11:43 +0530

    removeduplicate code added

---
+++ src/Array_PrimeClass/RemoveDuplicates.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class RemoveDuplicates {
+    static int removeDuplicate(int [] nums){
+        int k =1;
+        if(nums.length == 0){
+            return 0;
+        }
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        //printing array after removing
+        for(int i=0; i<nums.length; i++){
+            System.out.println(nums[i]);
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int [] nums = new int[10];
+        //insert array elements in acsending order
+        for(int i=0; i<nums.length; i++){
+            nums[i] = sc.nextInt();
+        }
+        System.out.println(removeDuplicate(nums));
+    }
+}
