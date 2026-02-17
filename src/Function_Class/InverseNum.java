7f792d17330469e2165fcee9bfd01bf432b51a9f
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-17 14:54:50 +0530

    inversenum

---
+++ src/Function_Class/InverseNum.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class InverseNum {
+    static int inversenum(int n){
+        int ans = 0;
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+    }
+}
