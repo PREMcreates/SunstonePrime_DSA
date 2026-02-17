2ab731b2874abd9eabc2e462ffe7667d7617983c
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-17 15:21:55 +0530

    inversenum

--- src/Function_Class/InverseNum.java
+++ src/Function_Class/InverseNum.java
@@ -1,11 +1,30 @@
+// import java.util.ArrayList;
 import java.util.*;
 public class InverseNum {
-    static int inversenum(int n){
-        int ans = 0;
-        return ans;
+    static void inversenum(int n){
+        int temp = n;
+        int dgt = 0;
+        //counting digits
+        while(n!=0){
+            dgt++;
+            n = n/10;
+        }
+        int [] ans = new int[dgt];
+        int i = 0;
+        while(temp!=0){
+            int l = temp%10;
+            ans[l-1] = i+1;
+            i++;
+            temp = temp/10;
+        }
+        for(i=dgt-1; i>=0; i--){
+            System.out.print(ans[i]);
+        }
+        
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        inversenum(n);
     }
 }
