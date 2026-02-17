ee53f6bcac6b0b8e6eb10c2cf1fd4c3764cc5473
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-18 00:32:45 +0530

    HappyNum

---
+++ src/Function_Class/HappyNumber_Leetcode.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+public class HappyNumber_Leetcode {
+
+    public static boolean isHappy(int n) {
+        int sum = 0;
+        int temp;
+        if(n==1){
+            return true;
+        }
+        else{
+             while (n != 1 && n!=4) {
+            sum = 0;
+            temp = n;
+            while (temp != 0) {
+                int ldgt = temp % 10;
+                sum = sum + (ldgt * ldgt);
+                temp = temp / 10;
+            }
+            n = sum;
+        }
+        }
+
+        if (sum == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(isHappy(n));
+    }
+}
