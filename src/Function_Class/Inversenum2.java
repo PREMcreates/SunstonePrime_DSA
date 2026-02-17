b744016a854284cc033e997b122e1c2b14944212
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-17 15:50:04 +0530

    inversenum2

--- src/Function_Class/Inversenum2.java
+++ src/Function_Class/Inversenum2.java
@@ -1,6 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 public class Inversenum2 {
     public static void main(String[] args) {
-        Scanner sc = 
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+
+        int i = 1;
+
+        while(n != 0){
+            int lastdgt = n % 10;
+            ans = ans + i * (int)(Math.pow(10, lastdgt - 1));
+            i++;
+            n = n / 10;
+        }
+
+        System.out.print(ans);
+        sc.close();
     }
 }
