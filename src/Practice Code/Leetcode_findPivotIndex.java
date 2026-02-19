a3afb9a80c9c3cbeaf64497b449cdfd2871e3799
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-19 15:29:27 +0530

    index

--- src/Practice Code/Leetcode_findPivotIndex.java
+++ src/Practice Code/Leetcode_findPivotIndex.java
@@ -1,25 +1,29 @@
-import java.util.*; 
+
+import java.util.*;
+
 public class Leetcode_findPivotIndex {
+
     public static int pivotIndex(int[] nums) {
         int total = 0;
-        for(int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             total += nums[i];
         }
         int leftsum = 0;
-        for(int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             int rightsum = total - leftsum - nums[i];
-            if(rightsum == leftsum){
+            if (rightsum == leftsum) {
                 return i;
             }
             leftsum += nums[i];
         }
         return -1;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int asize = sc.nextInt();
         int[] nums = new int[asize];
-        for(int i=0; i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             nums[i] = sc.nextInt();
         }
         int x = pivotIndex(nums);
