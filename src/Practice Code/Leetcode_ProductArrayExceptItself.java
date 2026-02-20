5faa83b1bf60b0062707c972cdccef3645bff7f7
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-20 23:41:31 +0530

    leetcode238 updated

--- src/Practice Code/Leetcode_ProductArrayExceptItself.java
+++ src/Practice Code/Leetcode_ProductArrayExceptItself.java
@@ -1,7 +1,17 @@
 import java.util.*;
 public class Leetcode_ProductArrayExceptItself {
+    static int ProductExceptself(int[] arr){
+        
+        return ans;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int 
+        System.out.println("Enter Array size: ");
+        int asize = sc.nextInt();
+        int[] arr = new int[asize];
+        for(int i=0; i<=arr.length; i++){
+            arr[i] = sc.nextInt();
+        }
+
     }
 }
