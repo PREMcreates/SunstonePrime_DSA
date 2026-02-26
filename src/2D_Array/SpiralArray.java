45732c6dc981ce3fb4082a627d1d461a621b7e09
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-02-26 15:32:40 +0530

    spiralarray

---
+++ src/2D_Array/SpiralArray.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class SpiralArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] a = new int[4][4];
+        //input array a
+        System.out.println("Enter elements in a: ");
+        for(int i=0; i<a.length; i++){
+            for(int j=0; j<a[i].length; j++){
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // traversing the array from as spiral
+        
+        
+    }
+}
