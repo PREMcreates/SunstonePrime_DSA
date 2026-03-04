fd9b51e7600e548f7d818fefdc4cb906babfda1f
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-04 23:13:15 +0530

    updated

--- src/Array_PrimeClass/Squareof_Array.java
+++ src/Array_PrimeClass/Squareof_Array.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class Squareof_Array {
-    static int[] SquareofArray(int[] arr){
+    static void  SquareofArray(int[] arr){
         int[] ans = new int[arr.length];
         for(int i=0; i<arr.length; i++){
             ans[i] = arr[i]*arr[i];
@@ -10,7 +10,7 @@ public class Squareof_Array {
         for(int i=0; i<arr.length; i++){
             System.out.println(ans[i]);
         }
-        return ans;
+        // return ans;
     }
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
@@ -19,6 +19,6 @@ public class Squareof_Array {
         for(int i=0; i<arr.length; i++){
             arr[i] = sc.nextInt();
         }
-        System.out.print(SquareofArray(arr));
+        SquareofArray(arr);
     }
 }
