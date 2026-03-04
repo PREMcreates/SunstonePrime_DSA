c7abda5ebb206c47513d59913b8057cd87aec3b6
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-04 18:58:46 +0530

    triplet

--- src/Array_PrimeClass/Triplets_Sum.java
+++ src/Array_PrimeClass/Triplets_Sum.java
@@ -1,15 +1,34 @@
 import java.util.*;
 public class Triplets_Sum {
 	static void Triplets(int[] arr, int target){
-			for(int i=0; i<arr.length; i++){
-				for(int j=i+1; j<arr.length; j++){
-					for(int k=j+1; k<arr.length; k++){
-						if(arr[i]+arr[j]+arr[k] == target){
-							System.out.println(arr[i]+" "+arr[j]+" "+"and "+arr[k]);
-						}
-					}
+			int n = arr.length;
+		for(int i=0; i<n-2; i++){
+			int left = i+1; 
+			int right = n-1;
+
+			while(left < right){
+				int sum = arr[i] + arr[left] + arr[right];
+
+				if(sum == target){
+					System.out.println(arr[i]+" "+arr[left]+" "+"and "+arr[right]);
+					left++;
+					right--;
+
+					//skip duplicates
+					while(left < right && arr[left] == arr[left-1])
+                        left++;
+
+                    while(left < right && arr[right] == arr[right+1])
+                        right--;
+				}
+				else if(sum < target){
+					left++;
+				}
+				else{
+					right--;
 				}
 			}
+		}
 	    }
     public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
