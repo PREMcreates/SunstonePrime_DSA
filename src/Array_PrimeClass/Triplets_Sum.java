c8619d4334ebb2fc8c72496ddf1a13089c9827d3
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-04 18:53:34 +0530

    tripletsum_updated

--- src/Array_PrimeClass/Triplets_Sum.java
+++ src/Array_PrimeClass/Triplets_Sum.java
@@ -5,18 +5,23 @@ public class Triplets_Sum {
 				for(int j=i+1; j<arr.length; j++){
 					for(int k=j+1; k<arr.length; k++){
 						if(arr[i]+arr[j]+arr[k] == target){
-							System.out.println(i+" "+j+" "+"and "+k);
+							System.out.println(arr[i]+" "+arr[j]+" "+"and "+arr[k]);
 						}
 					}
 				}
 			}
-	}
+	    }
     public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(); // size of array
 		int[] arr = new int[n];  // array of size n
+		//input of elements
+		for(int i=0; i<arr.length; i++){
+			arr[i] = sc.nextInt();
+		}
 		int target = sc.nextInt();   // target input for triplets
 
+		Arrays.sort(arr);
 		Triplets(arr, target);
 
     }
