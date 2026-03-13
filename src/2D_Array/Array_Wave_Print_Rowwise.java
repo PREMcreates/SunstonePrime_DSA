82b9336b99a97c2b4911be575f95f36ab9de56e6
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-14 00:19:55 +0530

    array wave traversal

--- src/2D_Array/Array_Wave_Print_Rowwise.java
+++ src/2D_Array/Array_Wave_Print_Rowwise.java
@@ -15,10 +15,18 @@ public class Array_Wave_Print_Rowwise {
         }
         //logic to print
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                System.out.print(arr[i][j]+" ");
+            if(i%2 == 0){
+                for(int j=0; j<n; j++){
+                    System.out.print(arr[i][j]+",");
+                }
+            }
+            else{
+                for(int j=n-1; j>=0; j--){
+                    System.out.print(arr[i][j]+",");
+                }
             }
         }
+        System.out.print("END");
 		
     }
 }
