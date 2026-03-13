416429e92ce8b33f1d3b7d71af76cfedf5b58a2d
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-13 23:57:08 +0530

    array travelsal

---
+++ src/2D_Array/Array_Wave_Print_Rowwise.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Array_Wave_Print_Rowwise {
+    public static void main(String args[]) {
+        // Your Code Here
+		Scanner sc = new Scanner(System.in);
+        
+		
+    }
+}
+
+
