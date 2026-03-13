2a5fa973fbbda8cc0fda34af66601f72cb3cd5eb
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-14 01:22:39 +0530

    array column wise

--- src/2D_Array/Array_Wave_Print_Rowwise.java
+++ src/2D_Array/Array_Wave_Print_Rowwise.java
@@ -14,15 +14,15 @@ public class Array_Wave_Print_Rowwise {
             }
         }
         //logic to print
-        for(int i=0; i<m; i++){
-            if(i%2 == 0){
-                for(int j=0; j<n; j++){
-                    System.out.print(arr[i][j]+",");
+        for(int j=0; j<n; j++){
+            if(j%2 == 0){
+                for(int i=0; i<m; i++){
+                    System.out.print(arr[i][j]+", ");
                 }
             }
             else{
-                for(int j=n-1; j>=0; j--){
-                    System.out.print(arr[i][j]+",");
+                for(int i=m-1; i>=0; i--){
+                    System.out.print(arr[i][j]+", ");
                 }
             }
         }
