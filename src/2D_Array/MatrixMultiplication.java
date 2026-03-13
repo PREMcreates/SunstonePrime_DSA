56cab8b24d5e600e3d086a8e6f3f6b75d4170ee6
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-13 14:50:55 +0530

    matrix multiplication

---
+++ src/2D_Array/MatrixMultiplication.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class MatrixMultiplication {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        int[][] a = new int[m][n];
+        int[][] b = new int[m][n];
+        int[][] result = new int[m][n];
+
+        //input for matrix a
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                a[i][j] = sc.nextInt();
+            }
+        }
+        //input for matrix b
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                b[i][j] = sc.nextInt();
+            }
+        }
+        //multiplication of both matrix a and b
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                result[i][j] = a[i][j] * b[i][j]; 
+            }
+        }
+        //printing the result
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(result[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
