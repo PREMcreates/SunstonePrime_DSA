dab45d547fb73742b4386b3d4ac0a8b83b616d5e
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-14 01:25:32 +0530

    search ele in matrix

---
+++ src/2D_Array/ElementSearch_inMatrix.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class ElementSearch_inMatrix {
+    public static int searchMatrix(int[][] matrix, int target) {
+        // Write your code here
+		for(int i=0; i<matrix.length; i++){
+            for(int j=0; j<matrix[0].length; j++){
+                if(matrix[i][j] == target){
+					return 1;
+				}
+            }
+        }
+		return 0;
+    }
+    public static void main(String args[]) {
+         Scanner scn=new Scanner(System.in);
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+        int[][] arr=new int[n][m];
+        for (int i=0; i<n; i++){
+               for(int j=0; j<m; j++){
+                     arr[i][j]=scn.nextInt();
+                }
+         }
+         int k = scn.nextInt();
+        System.out.print(searchMatrix(arr, k));
+    }
+    
+}
+
