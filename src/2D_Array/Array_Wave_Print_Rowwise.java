4d5e4fa810d753ec1426da1eed830521c4145b0d
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-14 00:01:36 +0530

    array 2d traversal

--- src/2D_Array/Array_Wave_Print_Rowwise.java
+++ src/2D_Array/Array_Wave_Print_Rowwise.java
@@ -3,7 +3,22 @@ public class Array_Wave_Print_Rowwise {
     public static void main(String args[]) {
         // Your Code Here
 		Scanner sc = new Scanner(System.in);
-        
+        int m = sc.nextInt();  // row
+        int n = sc.nextInt();  // column
+        int[][] arr = new int[m][n];
+
+        //taking input of nums of matrix
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        //printing output
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+        }
 		
     }
 }
