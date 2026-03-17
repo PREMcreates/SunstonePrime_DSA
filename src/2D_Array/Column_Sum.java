f0ae2a1bdcde4848a8e2ab687f1faf54518f36a3
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-17 12:23:53 +0530

    coulmn_sum

--- src/2D_Array/Column_Sum.java
+++ src/2D_Array/Column_Sum.java
@@ -4,6 +4,25 @@ public class Column_Sum {
 		Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[][] arr = new int[n][n];
-        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        //logic
+        int sum = 0, ans = 0, index = 0;
+        int i, j;
+        for(j=0; j<n; j++){
+            for(i=0; i<n; i++){
+                sum = sum + arr[i][j];
+            }
+            if(sum > ans){
+                ans = sum;
+                index = j;
+            }
+        }
+        System.out.println(j+" ");
+        System.out.print(ans);
+
     }
 }
