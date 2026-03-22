820daf6c4b0652d27f9369c3f15826f223fc203f
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-03-23 00:50:06 +0530

    final_Insertion_sort_codeUpdated

--- src/Sorting_Class/Insertion_Sort.java
+++ src/Sorting_Class/Insertion_Sort.java
@@ -4,29 +4,37 @@ public class Insertion_Sort {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[5];
-        //input array
-        System.out.println("Before Sorting");
+
+        // input
+        System.out.println("Enter elements:");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
 
-        //insertion sort
+        System.out.println("Before Sorting:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
+        // insertion sort
         int n = arr.length;
-        for(int i=1; i<n; i++){
+        for (int i = 1; i < n; i++) {
             int key = arr[i];
-            int j = i-1;
-            while(arr[j]>key && j>=0){
-                arr[j+1] = arr[j];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
                 j--;
             }
-            arr[j+1] = key;
-        }
-        
 
-        //printing sorted array
-        System.out.println("After Sorting");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+            arr[j + 1] = key;
         }
+
+        // output
+        System.out.println("\nAfter Sorting:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+
     }
 }
