df4021493626c33a9afa26a2ae6fc3d9ae2ae902
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-04-04 18:39:24 +0530

    lps leetcode 5

---
+++ src/Leetcode/Longest_Palindrome_Substring.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Longest_Palindrome_Substring {
+    public static String lps(String s){
+        
+        return s;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        System.out.println(lps(s));
+
+    }
+}
